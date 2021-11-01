b0a16d67d1bf676ddc4a60542adc071c68cb976f
Falk Schoenfeld <schoenfeld@magazino.eu>
2021-11-01 22:26:56 +0100

    i3 config movement

---
+++ i3/i3_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# linkcmd="/bin/bash ./scripts/lnnew.sh"
+
+echo "Setting up files"
+
+# mkdir -p ~/.config/i3
+# $linkcmd i3/i3config ~/.config/i3/config
+
+ln -s ~/auto-setup/i3 ~/.config/i3
