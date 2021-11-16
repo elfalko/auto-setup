61bba1d98e47cad2aaec5ea04c4af37c3f3da405
Falk Schoenfeld <schoenfeld@magazino.eu>
2021-11-16 09:49:31 +0100

    improved multihead switching

--- sway/switch_multihead.sh
+++ sway/switch_multihead.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
 # script to switch dual screen setup
-# while possible to do so with just config, this allows switching back and forth
+# * remembers which side you are currently on
+# * can handle auto-back-and-forth or go to unfocused side on repress
+# 
+
+# DEBUG_MODE=true
 
 SEPARATOR="-"
 PRIMARY="-1"
 SECONDARY="-2"
 TMPFILE=~/.swaywsstuff
 
+AUTO_BACK_AND_FORTH=true
+
 WS_CURRENT=$(swaymsg -t get_workspaces | jq --raw-output '.[] | try select(.focused == true) | .name')
-WS_CURRENT="${WS_CURRENT%-*}"
+WS_CURRENT_SIDE_FOCUSED=$SEPARATOR${WS_CURRENT##*$SEPARATOR}
+WS_CURRENT="${WS_CURRENT%$SEPARATOR*}"
+
 WS_LAST=$(cat $TMPFILE)
 
-echo "Requested WS $1"
+WS_CURRENT_SIDE_UNFOCUSED=$SECONDARY
+if [ $WS_CURRENT_SIDE_FOCUSED == $WS_CURRENT_SIDE_UNFOCUSED ]; then
+    WS_CURRENT_SIDE_UNFOCUSED=$PRIMARY
+fi
+
 
 if [ $1 == $WS_CURRENT ]; then
-    echo "current ws $WS_CURRENT matches, going to last $WS_LAST"
-    swaymsg "workspace $WS_LAST-1; workspace $WS_LAST-2"
+    if [ $AUTO_BACK_AND_FORTH == true ];then
+        # echo "current ws $WS_CURRENT matches, going to last $WS_LAST"
+        # echo "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
+        swaymsg "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
+    else
+        # echo "current ws $WS_CURRENT matches, going to other screen $WS_CURRENT_SIDE_UNFOCUSED"
+        swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED"
+    fi
 else
-    echo "going to $1"
-    swaymsg "workspace $1-1; workspace $1-2"
+    # echo "going to $1"
+    # echo "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
+    swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
 fi
 
 echo $WS_CURRENT>$TMPFILE
