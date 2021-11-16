12a811e802f6e938fb270a6de9e4d11f89885422
Falk Schoenfeld <schoenfeld@magazino.eu>
2021-11-16 15:45:26 +0100

    slims down regolith setup to relevant

--- regolith_setup.sh
+++ regolith_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./scripts/ubuntu-basics.sh
+# ./scripts/ubuntu-basics.sh
 
 # actual setup
 echo "Setting up files"
@@ -8,39 +8,10 @@ echo "Setting up files"
 # links to a resource file 
 # $1: link target in resources ./resource/$1
 # $2: name of link
-lnnew(){
-    echo ""
-    if [ -f "$2" ]; then
-        echo "- $2 exists, either delete or run"
-        echo "    mv $2 $2.old"
-    else
-	if [ -L "$2" ]; then
-	    echo "- link $1 already exists, done"
-	else
-	    echo "- linking $1"
-	    ln -s "$(pwd)/resources/$1" "$2" 
-	fi
-    fi
-}
+linkcmd="/bin/bash ./scripts/lnnew.sh"
 
-mkdir -p ~/.config/tmux
-lnnew tmux.conf ~/.config/tmux/tmux.conf 
-
-mkdir -p ~/.config/nvim
-lnnew init.vim ~/.config/nvim/init.vim
-
-lnnew pre-commit ./.git/hooks/pre-commit
-
-lnnew .bash_aliases ~/.bash_aliases
-
-lnnew .inputrc ~/.inputrc
-
-lnnew .gitconfig ~/.gitconfig
-
-lnnew .Xmodmap ~/.Xmodmap
-
-lnnew .Xresources ~/.config/regolith/Xresources
+$linkcmd Xresources ~/.config/regolith/Xresources
 
 echo ""
 
-./scripts/vbox.sh
+# ./scripts/vbox.sh
