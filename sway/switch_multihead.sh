3c5bb56da524d408842cd4f9ea045603438c6a72
Falk Fairfield <falk.schoenfeld@tum.de>
2021-12-18 23:59:43 +0100

    cleans up switch_multihead script

--- sway/switch_multihead.sh
+++ sway/switch_multihead.sh
@@ -4,13 +4,13 @@
 # * remembers which side you are currently on
 # * can handle auto-back-and-forth or go to unfocused side on repress
 # 
-
-# DEBUG_MODE=true
+# requires jq 
 
 SEPARATOR="-"
 PRIMARY="-1"
 SECONDARY="-2"
-TMPFILE=~/.swaywsstuff
+TMPDIR=~/.cache/sway
+TMPFILE=$TMPDIR/last_workspace
 
 AUTO_BACK_AND_FORTH=true
 
@@ -18,7 +18,10 @@ WS_CURRENT=$(swaymsg -t get_workspaces | jq --raw-output '.[] | try select(.focu
 WS_CURRENT_SIDE_FOCUSED=$SEPARATOR${WS_CURRENT##*$SEPARATOR}
 WS_CURRENT="${WS_CURRENT%$SEPARATOR*}"
 
-WS_LAST=$(cat $TMPFILE)
+mkdir -p $TMPDIR
+if [ -f $TMPFILE ]; then
+    WS_LAST=$(cat $TMPFILE)
+fi
 
 WS_CURRENT_SIDE_UNFOCUSED=$SECONDARY
 if [ $WS_CURRENT_SIDE_FOCUSED == $WS_CURRENT_SIDE_UNFOCUSED ]; then
@@ -28,16 +31,11 @@ fi
 
 if [ $1 == $WS_CURRENT ]; then
     if [ $AUTO_BACK_AND_FORTH == true ];then
-        # echo "current ws $WS_CURRENT matches, going to last $WS_LAST"
-        # echo "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
         swaymsg "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
     else
-        # echo "current ws $WS_CURRENT matches, going to other screen $WS_CURRENT_SIDE_UNFOCUSED"
         swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED"
     fi
 else
-    # echo "going to $1"
-    # echo "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
     swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
 fi
 
