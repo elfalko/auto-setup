98f163fd022d71f5c3507789e91430a32844b105
Falk Fairfield <falk.schoenfeld@tum.de>
2021-12-18 23:56:08 +0100

    fix i3lock path

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -2,6 +2,6 @@
 
 # wrapper for i3lock-color, which does not accept a config file apparently
 TEXT="↑↑↓↓←→←→BA"
-BG="~/pics/wallpapers/bounty_hunters_by_wojtekfus-dar6xah.png"
+BG="$HOME/pics/wallpapers/bounty_hunters_by_wojtekfus-dar6xah.png"
 
 i3lock --greeter-text="$TEXT" --wrong-text="FAIL" -i $BG -F --keylayout 0 -f --pass-media-keys --pass-screen-keys --pass-volume-keys -e --greeter-color=ffffffff
