2c522955faf2824c9042a07bb71848f3852d973f
Falk Schoenfeld <schoenfeld@magazino.eu>
2021-12-19 00:12:36 +0100

    vnc config for i3

---
+++ feh/feh_default_local_background.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set a fancy background on the home screen
+# and a normal one on all attached
+
+BGPATH=$HOME/setup/wallpapers/default.jpg
+
+feh --no-fehbg --auto-rotate --bg-max BGPATH
