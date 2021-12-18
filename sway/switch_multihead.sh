63157317dcd7d73893336684acf0b5a4d31d38a1
Falk Schoenfeld <schoenfeld@magazino.eu>
2021-12-19 00:06:28 +0100

    sway multihead script now also works for i3

--- sway/switch_multihead.sh
+++ sway/switch_multihead.sh
@@ -9,13 +9,20 @@
 
 source ~/auto-setup/sway/ws_config.sh
 
+if [ -z $SWAYSOCK ]; then
+    WM_MSG=swaymsg
+else
+    WM_MSG=i3-msg
+fi
+
 AUTO_BACK_AND_FORTH=true
 
-WS_CURRENT=$(swaymsg -t get_workspaces | jq --raw-output '.[] | try select(.focused == true) | .name')
+WS_CURRENT=$($WM_MSG -t get_workspaces | jq --raw-output '.[] | try select(.focused == true) | .name')
 WS_CURRENT_SIDE_FOCUSED=$SEPARATOR${WS_CURRENT##*$SEPARATOR}
 WS_CURRENT="${WS_CURRENT%$SEPARATOR*}"
 
 WS_LAST=$(cat $TMPFILE)
+
 
 WS_CURRENT_SIDE_UNFOCUSED=$SECONDARY
 if [ $WS_CURRENT_SIDE_FOCUSED == $WS_CURRENT_SIDE_UNFOCUSED ]; then
@@ -27,15 +34,15 @@ if [ $1 == $WS_CURRENT ]; then
     if [ $AUTO_BACK_AND_FORTH == true ];then
         # echo "current ws $WS_CURRENT matches, going to last $WS_LAST"
         # echo "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
-        swaymsg "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
+        $WM_MSG "workspace $WS_LAST$WS_CURRENT_SIDE_UNFOCUSED; workspace $WS_LAST$WS_CURRENT_SIDE_FOCUSED"
     else
         # echo "current ws $WS_CURRENT matches, going to other screen $WS_CURRENT_SIDE_UNFOCUSED"
-        swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED"
+        $WM_MSG "workspace $1$WS_CURRENT_SIDE_UNFOCUSED"
     fi
 else
     # echo "going to $1"
     # echo "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
-    swaymsg "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
+    $WM_MSG "workspace $1$WS_CURRENT_SIDE_UNFOCUSED; workspace $1$WS_CURRENT_SIDE_FOCUSED"
 fi
 
 echo $WS_CURRENT>$TMPFILE
