d9f8afa955e89c375749155048688e2b444c2cbc
Falk Schoenfeld <schoenfeld@magazino.eu>
2022-01-12 11:05:46 +0100

    improved gld scripts

--- scripts/get_latest_download.sh
+++ scripts/get_latest_download.sh
@@ -1,6 +1,29 @@
-#!/bin/bash
+#/bin/bash
 
-DOWNLOADDIR="$HOME/Downloads"
-LATEST="$(ls -t $DOWNLOADDIR | head -n1)"
-echo "Getting $LATEST"
-mv "$DOWNLOADDIR/$LATEST" .
+# set -euxo pipefail
+
+get_latest_firefox_download(){
+    DOWNLOADDIR="/tmp/mozilla_${USER}0"
+    get_latest_file $DOWNLOADDIR
+}
+
+get_latest_download(){
+    DOWNLOADDIR="$HOME/Downloads"
+    get_latest_file $DOWNLOADDIR
+}
+
+get_latest_file(){
+    LATEST="$(ls -t $1 | head -n1)"
+    if [ -z "$LATEST" ]; then
+        echo "No file found at $1"
+        exit 1
+    else
+        echo "Getting $LATEST"
+        mv "$DOWNLOADDIR/$LATEST" .
+    fi
+    exit 0
+}
+
+export -f get_latest_file
+export -f get_latest_download
+export -f get_latest_firefox_download
