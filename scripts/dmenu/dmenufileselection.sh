705e587e129e465feb63e208966b63beb447419f
Falk Schoenfeld <schoenfeld@magazino.eu>
2022-02-23 10:29:30 +0100

    WIP: dmenuscript should be able to read lists of command from files

--- scripts/dmenu/dmenufileselection.sh
+++ scripts/dmenu/dmenufileselection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -exo pipefail
 
 print_help(){
     echo "Dmenu file selection"
@@ -19,14 +19,27 @@ fi
 # check if dmenu lives
 pgrep -x dmenu && exit 1
 
-TARGET=$1
+TARGET="$1"
 
-ENTRIES=$(ls -A $1)
+if [ -d $TARGET ]; then
+    echo "found directory"
+    ENTRIES="$(ls -A $1)"
+else if [ -f $TARGET ]; then
+    echo "found file"
+    ENTRIES="$(cat $TARGET)"
+else
+    echo "$TARGET is neither file nor directory, can't handle this"
+    print_help
+    exit 1
+fi
+
+echo "Entries:"
+echo "$ENTRIES"
 
 [ -z "$ENTRIES" ] && exit 1
 
 if [ ${#ENTRIES[@]} -eq 1 ]; then 
-    CHOSEN=$ENTRIES
+    CHOSEN="$ENTRIES"
 else
     CHOSEN=$(echo "$ENTRIES" | dmenu -i -p "Options:")
 fi
