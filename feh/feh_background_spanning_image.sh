270822fe344d6e6a207cdb6ebe4f30955c8f14e2
Falk Schoenfeld <schoenfeld@magazino.eu>
2022-03-25 10:20:15 +0100

    WIP: sets terminal colors but breaks spanning bg

--- feh/feh_background_spanning_image.sh
+++ feh/feh_background_spanning_image.sh
@@ -3,4 +3,12 @@
 # set a fancy background on the home screen
 # and a normal one on all attached
 
-feh --no-fehbg --auto-rotate --bg-max --randomize --no-xinerama /home/fsc/setup/wallpapers
+PICFOLDER=$1
+PIC=$(ls $1 | sort -R | tail -n 1)
+
+notify-send "$PICFOLDER/$PIC"
+# set background
+feh --no-fehbg --auto-rotate --bg-max --no-xinerama "$PICFOLDER/$PIC"
+# set terminal colors
+setsid wal -i "$PICFOLDER/$PIC"
+
