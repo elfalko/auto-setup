83526145ddc533e9eff9961064330ef3f95c5823
Falk Schoenfeld <schoenfeld@magazino.eu>
2022-03-25 10:33:40 +0100

    fixes background setter

--- feh/feh_background_spanning_image.sh
+++ feh/feh_background_spanning_image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# set a fancy background on the home screen
-# and a normal one on all attached
+# set a fancy background across screens
 
 PICFOLDER=$1
 PIC=$(ls $1 | sort -R | tail -n 1)
 
-notify-send "$PICFOLDER/$PIC"
+# set terminal colors
+setsid wal -i "$PICFOLDER/$PIC" 
+# notify-send "$PICFOLDER/$PIC"
+sleep 1s
 # set background
 feh --no-fehbg --auto-rotate --bg-max --no-xinerama "$PICFOLDER/$PIC"
-# set terminal colors
-setsid wal -i "$PICFOLDER/$PIC"
 
