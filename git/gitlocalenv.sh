18bb8feec6921d5fc76ce468349ec76b1c04df8f
Falk Schoenfeld <falk.schoenfeld@work-microwave.com>
2023-02-20 12:46:51 +0100

    moves git email to a more local setup variable

---
+++ git/gitlocalenv.sh
@@ -0,0 +1,3 @@
+export GIT_AUTHOR_EMAIL="falk.fairfield@gmail.com"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+export GIT_EDITOR=$EDITOR
---
+++ git/setup.sh
@@ -0,0 +1,4 @@
+ln -s ~/auto-setup/git/gitconfig ~/.gitconfig
+mkdir -p ~/.bash_individual_aliases/
+cp ~/auto-setup/git/gitlocalenv.sh
+$EDITOR ~/auto-setup/git/gitlocalenv.sh 
