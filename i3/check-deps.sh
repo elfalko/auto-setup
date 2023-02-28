cccc0d2d173729133d8a4d207a75946c080c7778
Falk Schoenfeld <falk.schoenfeld@work-microwave.com>
2023-02-28 16:18:00 +0100

    auto install deps

---
+++ i3/check-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# script to check if programs are missing
+
+set -euxo pipefail
+
+distro="$(lsb_release -d)"
+pkgmanager="#package manager"
+echo "$distro" | grep -i ubuntu && pkgmanager="sudo apt install"
+echo "$distro" | grep -i manjaro && pkgmanager="pamac install"
+echo "$distro" | grep -i arch && pkgmanager="sudo pacman -Sy"
+
+function find_or_install(){
+    which "$1" || ${pkgmanager} $1
+}
+
+find_or_install jq
+find_or_install polkit
