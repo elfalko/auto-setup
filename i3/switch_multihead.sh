048dd21d69657549641545e6b8db669e53d973fe
Falk Schoenfeld <falk.schoenfeld@work-microwave.com>
2023-02-28 16:15:38 +0100

    improved multihead for single screens

--- i3/switch_multihead.sh
+++ i3/switch_multihead.sh
@@ -5,6 +5,11 @@
 # * can handle auto-back-and-forth or go to unfocused side on repress
 # 
 # requires jq 
+#
+# edge cases: 
+# - [x] single screen needs no suffix
+# - [ ] specifc suffixes: 1-Mail
+# - [ ] auto back and forth between ws with suffix and without
 
 source ~/auto-setup/sway/ws_config.sh
 
@@ -30,17 +35,22 @@ if [ $WS_NEW == $WS_CURRENT ]; then
     WS_NEW=$WS_LAST
 fi
 
-#idea: call all workspaces, then call focused again
-for (( MON_C=1; MON_C<=$MON_NUM; MON_C++ )); do
-    if [ "-$MON_C" != $MON_FOCUS ]; then
-        CMD="workspace $WS_NEW$SEPARATOR$MON_C;"
-        # notify-send "$CMD"
-        $WM_MSG "$CMD"
-    fi
-done
+if [ $MON_NUM -gt 1 ]; then
+    #idea: call all workspaces, then call focused again
+    for (( MON_C=1; MON_C<=$MON_NUM; MON_C++ )); do
+        if [ "-$MON_C" != $MON_FOCUS ]; then
+            CMD="workspace $WS_NEW$SEPARATOR$MON_C;"
+            notify-send "$CMD"
+            $WM_MSG "$CMD"
+        fi
+    done
 
-CMD="workspace $WS_NEW$MON_FOCUS;"
-# notify-send "$CMD"
+    CMD="workspace $WS_NEW$MON_FOCUS;"
+else
+    CMD="workspace $WS_NEW"
+fi
+
+notify-send "$CMD"
 sleep 0.1
 $WM_MSG "$CMD"
 
