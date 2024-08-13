e8969e3e8f6d83db73ce397e9d96f759c7fbed91
Falk Schoenfeld <falk.schoenfeld@balluff.com>
2024-08-13 10:31:23 +0200

    timew default setup

---
+++ timew/setup.sh
@@ -0,0 +1,7 @@
+timew config exclusions.monday    '<7:00 12:00-12:45 >20:00' :yes
+timew config exclusions.tuesday   '<7:00 12:00-12:45 >20:00' :yes
+timew config exclusions.wednesday '<7:00 12:00-12:45 >20:00' :yes
+timew config exclusions.thursday  '<7:00 12:00-12:45 >20:00' :yes
+timew config exclusions.friday    '<7:00 12:00-12:45 >20:00' :yes
+timew config exclusions.saturday  '>0:00'             :yes
+timew config exclusions.sunday    '>0:00'             :yes
