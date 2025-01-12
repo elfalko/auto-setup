322134c0841e29e7b11709a60b034462ede175c4
Falk Schoenfeld <falk.schoenfeld@balluffstm.com>
2025-01-12 23:16:53 +0100

    improve git and trizen

--- arch/trizen_safer.sh
+++ arch/trizen_safer.sh
@@ -6,7 +6,7 @@ PACLOG=/var/log/pacman.log
 sudo mkdir -p /var/log/pacman.d/ &&
 sudo mv $PACLOG /var/log/pacman.d/pacman.$(date +%y%m%d-%H%M%S).log &&
 trizen -Sy --noconfirm archlinux-keyring && 
-trizen -Syu "$@"; 
+trizen -Syu --needed "$@"; 
 #some paclog highlighting
 grep -i warn $PACLOG
 grep -i pacnew $PACLOG
--- git/startagent.sh
+++ git/startagent.sh
@@ -5,6 +5,8 @@ if [ -z $SSH_AGENT_PID ]; then
     eval $(ssh-agent -s)
     chmod 600 $SSHDIR/id_rsa
     ssh-add $SSHDIR/id_rsa
+    chmod 600 $SSHDIR/github_elfalko
+    ssh-add $SSHDIR/github_elfalko
 else
     echo "SSH_AGENT_PID: $SSH_AGENT_PID - not doing anything"
 fi
