26b153d5cc4b42f2a504a053ea229d1c4b65b1fa
Falk Schoenfeld <falk.schoenfeld@microsys.de>
2025-03-25 13:07:33 +0100

    git config with include to exclude email

--- git/setup.sh
+++ git/setup.sh
@@ -1,4 +1,10 @@
-ln -s ~/auto-setup/git/gitconfig ~/.gitconfig
-mkdir -p ~/.bash_individual_aliases/
+#!/bin/bash
+#
+cat >> ~/.gitconfig << EOF
+[include]
+  path = ~/auto-setup/git/gitconfig
+EOF
+
+# ln -s ~/auto-setup/git/gitconfig ~/.gitconfig
 # cp ~/auto-setup/git/gitlocalenv.sh
-$EDITOR ~/auto-setup/git/gitlocalenv.sh 
+$EDITOR ~/.gitconfig ~/auto-setup/git/gitlocalenv.sh 
