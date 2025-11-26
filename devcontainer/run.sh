11590ce215a27ed758cc4787c73e3e7b9eb74f3c
Falk Schoenfeld <falk.fairfield@gmail.com>
2025-11-26 15:00:06 +0100

    included machine specific aliases in fdev

--- devcontainer/run.sh
+++ devcontainer/run.sh
@@ -2,11 +2,14 @@
 
 # assumption: this script is run from the folder where editing should happen
 
+DEVUSER=dev
+
 echo "cd ../auto-setup/; export HOME=/home/dev; . setup.sh; cd -; bash"
 docker run \
-  --user "$(id -u):dev" \
-  --volume="$(pwd)":/home/dev/ws \
-  --volume="$HOME/auto-setup/":/home/dev/auto-setup \
+  --user "$(id -u):${DEVUSER}" \
+  --volume="$(pwd)":/home/${DEVUSER}/ws \
+  --volume="$HOME/auto-setup/":/home/${DEVUSER}/auto-setup \
+  --volume="$HOME/.bash_individual_aliases/":/home/${DEVUSER}/.bash_individual_aliases \
   -it falk/fdev:latest \
   /bin/bash -l
 
